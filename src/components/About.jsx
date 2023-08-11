24944f17f4cecac32e72c882b948eda8ab2d42cb
RoshanMajo18 <roshanmajo18@gmail.com>
2023-08-11 15:28:39 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,7 +40,7 @@ const About = () => {
       variants={fadeIn("","",0.1,1)}
       className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm pursuing my BTech in Computer Science. My skills include 
+        I'm pursuing my BTech in Computer Science at College of Engineering Trivandrum. My skills include 
         React JS, HTML, JavaScript, CSS, TailwindCSS, Firebase, C, CPP etc. 
         I'm a quick learner and collaborate closely with my teamates to finish 
         a projet in the most efficient manner and in less time.
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,8 +17,8 @@ const Hero = () => {
 
         </h1>
         <p className={`${styles.heroSubText}  mt-2 text-white-100`}>
-        I develop 3D visuals,user <br className='sm:block hidden' />
-        interfaces and web applictions
+        I am a MERN stack developer <br className='sm:block hidden' />
+       
 
         </p>
         
